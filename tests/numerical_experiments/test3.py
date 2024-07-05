0ad23cfd57af2d4c01bb058d37cf74fd03aa8512
Ekaterina Mozhegova <mozhegova04@gmail.com>
2024-07-05 09:23:20 +0300

    More tests with experiments

--- tests/numerical_experiments/test3.py
+++ tests/numerical_experiments/test3.py
@@ -8,44 +8,28 @@ model = mujoco.MjModel.from_xml_path('example.xml')
 data = mujoco.MjData(model)
 measurements_list = []
 
-# def collision_handler(model_, data_):
-#     if data_.contact.geom.size != 0:
-#         data_.qpos += [np.deg2rad(30, 30, 30)]
-#         # if data.contact:
-#         print("collision occurred")
-#         mujoco.mj_step(model_, data_)
-#         break
-
-
 with mujoco.viewer.launch_passive(model, data) as viewer:
     # Close the viewer automatically after 30 wall-seconds.
     start = time.time()
-    while viewer.is_running() and time.time() - start < 30:
+    while viewer.is_running() and time.time() - start < 10:
         step_start = time.time()
 
-        for q1 in range(0, 360, 5):
-            for q2 in range(0, 360, 5):
-                for q3 in range(0, 360, 5):
+        for q1 in range(0, 360, 25):
+            for q2 in range(0, 360, 25):
+                for q3 in range(0, 360, 25):
+                    # mujoco.mj_step(model, data)
+                    data.qpos = [np.deg2rad(q1), np.deg2rad(q2), np.deg2rad(q3)]
+                    data.qvel = [0, 0, 0]
+                    data.qacc = [0, 0, 0]
                     mujoco.mj_step(model, data)
+                    mujoco.mj_inverse(model, data)
 
                     if data.contact.geom.size > 0:
                         print("collision occurred")
-                        # mujoco.mj_step(model, data)
-                        # data.qpos += [np.deg2rad(30), np.deg2rad(30), np.deg2rad(30)]
-                        break
-
-                    data.qpos = [np.deg2rad(q1), np.deg2rad(q2), np.deg2rad(q3)]
-
-                    mujoco.mj_step(model, data)
-
-                    data.qvel = [0, 0, 0]
-                    data.qacc = [0, 0, 0]
-                    mujoco.mj_inverse(model, data)
-                    tau = data.qfrc_inverse
-                    measurements_list.append([q1, q2, q3, tau[0], tau[1], tau[2]])
-                    # print("tau: ", data.qfrc_inverse)
-
-                    mujoco.mj_step(model, data)
+                    else:
+                        tau = data.qfrc_inverse
+                        measurements_list.append([q1, q2, q3, tau[0], tau[1], tau[2]])
+                        # print("tau: ", data.qfrc_inverse)
 
                     viewer.sync()
 
@@ -53,8 +37,7 @@ with mujoco.viewer.launch_passive(model, data) as viewer:
                     if time_until_next_step > 0:
                         time.sleep(time_until_next_step)
 
-
-with open('angles_and_torques.csv', 'w', newline='') as file:
+with open('angles_and_torques5.csv', 'w', newline='') as file:
     writer = csv.writer(file)
     writer.writerow(["Angle1", "Angle2", "Angle3", "Tau1", "Tau2", "Tau3"])
     writer.writerows(measurements_list)
